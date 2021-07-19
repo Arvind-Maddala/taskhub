fa1a01d5cce474ca161a6ef9ac69fa0a41339868
unknown <arvindprince10@gmail.com>
2021-07-19 21:22:19 +0530

    draggable updated

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,32 +1,21 @@
 const cards = [
-  {
-    id: 'card-1',
-    title: 'Learning how to cook',
-  },
-  {
-    id: 'card-2',
-    title: 'Making sandwich',
-  },
-  {
-    id: 'card-3',
-    title: 'Taking the trash out',
-  },
+ 
 ];
 
 const data = {
   lists: {
     'list-1': {
       id: 'list-1',
-      title: 'Todo',
+      title: 'List-1',
       cards,
     },
     'list-2': {
       id: 'list-2',
-      title: 'Doing',
+      title: 'List-2',
       cards: [],
     },
   },
-  listIds: ['list-1', 'list-2'],
+  listIds: ['list-1'],
 };
 
 export default data;
